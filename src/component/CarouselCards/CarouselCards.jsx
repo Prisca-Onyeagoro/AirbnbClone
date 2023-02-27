7afa2810b59d0c97c26aa415a9946d7c09918486
prisca onyeagoro <priscaonyeagoro@gmail.com>
2023-02-27 23:02:57 +0100

    continuation of airbnb project clone

--- src/component/CarouselCards/CarouselCards.jsx
+++ src/component/CarouselCards/CarouselCards.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import SwipeableViews from 'react-swipeable-views-react-18-fix';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import StarIcon from '@mui/icons-material/Star';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { Box } from '@mui/material';
-import { fixedIcon } from '../../Themes/CommonThemes';
+import Favorite from '@mui/icons-material/Favorite';
+import { Box, Button, MobileStepper, Typography } from '@mui/material';
+import {
+  fixedIcon,
+  carouselImage,
+  carouselDot,
+  flexBetween,
+  dFlex,
+} from '../../Themes/CommonThemes';
+import '../Carousal.css';
 const CarouselCards = ({ location }) => {
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = location.locationImages.length; // to know maximum number of dots to be on the carousel
@@ -26,7 +34,79 @@ const CarouselCards = ({ location }) => {
         position: 'relative',
       }}
     >
-      <Box sx={fixedIcon}></Box>
+      <Box sx={fixedIcon}>
+        <Favorite size={24} color="#fff" />
+      </Box>
+      {location.locationImages.length && (
+        <SwipeableViews
+          axis={'x'}
+          index={activeStep}
+          onChange={handleStepChange}
+          enableMouseEvents
+        >
+          {location.locationImages.map((step, index) => {
+            return (
+              <div key={step.id}>
+                <Box
+                  component="img"
+                  sx={{ carouselImage }}
+                  src={step.url}
+                  alt={step.id}
+                ></Box>
+              </div>
+            );
+          })}
+        </SwipeableViews>
+      )}
+      <Box>
+        <MobileStepper
+          sx={{ background: 'transparent' }}
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            <Button
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+              sx={carouselDot}
+            >
+              <KeyboardDoubleArrowRightIcon />
+            </Button>
+          }
+          backButton={
+            <Button
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+              sx={carouselDot}
+            >
+              <KeyboardDoubleArrowLeftIcon />
+            </Button>
+          }
+        />
+      </Box>
+      <Box sx={flexBetween}>
+        <Box sx={{ mt: 2 }}>
+          <Typography component={'h3'}>{location.location}</Typography>
+          <Typography component={'h4'}>{location.days}</Typography>
+          <Typography component={'h5'}>{location.price}</Typography>
+        </Box>
+        <Box sx={{ mt: 2 }}>
+          <Box sx={dFlex}>
+            {location.isNew ? (
+              <>
+                <Typography component={'h5'}>New</Typography>
+                <StarIcon size={18} />
+              </>
+            ) : (
+              <>
+                <Typography component={'h5'}>{location.rating}</Typography>
+              </>
+            )}
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
--- src/component/LocationCards/LocationCards.jsx
+++ src/component/LocationCards/LocationCards.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { locations as cardLocations } from '../Data/MockData';
 import { Box, Grid } from '@mui/material';
+import CarouselCards from '../CarouselCards/CarouselCards';
+import '../Carousal.css';
 
 const LocationCards = () => {
   const [cards] = React.useState(cardLocations);
@@ -10,8 +12,8 @@ const LocationCards = () => {
       <Grid container rowSpacing={3} columnSpacing={3}>
         {cards.map((location) => {
           return (
-            <Grid key={location.id} xs={12} sm={2} md={4} lg={3}>
-              {location.location}
+            <Grid key={location.id} xs={12} sm={4} md={4} lg={3} item>
+              <CarouselCards location={location} />
             </Grid>
           );
         })}
